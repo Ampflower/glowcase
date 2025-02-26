ef491cda13e71a573bd5cc06bcf38b063e8b364d
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2025-02-26 18:58:09 -0500

    - Increased pitch and yaw increment/decrement amount from 1 to 15.

--- src/main/java/dev/hephaestus/glowcase/client/gui/screen/ingame/ItemDisplayBlockEditScreen.java
+++ src/main/java/dev/hephaestus/glowcase/client/gui/screen/ingame/ItemDisplayBlockEditScreen.java
@@ -22,6 +22,9 @@ public class ItemDisplayBlockEditScreen extends GlowcaseScreen {
 	private ButtonWidget increasePitch;
 	private ButtonWidget decreaseYaw;
 	private ButtonWidget increaseYaw;
+
+	private final float pitchYawChange = 15F * ((float)Math.PI/180F);
+
 	public ItemDisplayBlockEditScreen(ItemDisplayBlockEntity displayBlock) {
 		this.displayBlock = displayBlock;
 	}
@@ -85,25 +88,25 @@ public class ItemDisplayBlockEditScreen extends GlowcaseScreen {
 			}).dimensions(110, 90, 20, 20).build();
 
 			this.decreasePitch = ButtonWidget.builder(Text.literal("-"), action -> {
-				this.displayBlock.pitch -= (float) Math.max(0, 0.0174533F);
+				this.displayBlock.pitch -= (float) Math.max(0, pitchYawChange);
 				this.displayBlock.pitch = MathHelper.clamp(this.displayBlock.pitch, -6.28319F,6.28319F);
 				editItemDisplayBlock();
 			}).dimensions(90, 120, 20, 20).build();
 
 			this.increasePitch = ButtonWidget.builder(Text.literal("+"), action -> {
-				this.displayBlock.pitch += 0.0174533F;
+				this.displayBlock.pitch += pitchYawChange;
 				this.displayBlock.pitch = MathHelper.clamp(this.displayBlock.pitch, -6.28319F,6.28319F);
 				editItemDisplayBlock();
 			}).dimensions(110, 120, 20, 20).build();
 
 			this.decreaseYaw = ButtonWidget.builder(Text.literal("-"), action -> {
-				this.displayBlock.yaw -= (float) Math.max(0, 0.0174533);
+				this.displayBlock.yaw -= (float) Math.max(0, pitchYawChange);
 				this.displayBlock.yaw = MathHelper.clamp(this.displayBlock.yaw, -6.28319F,6.28319F);
 				editItemDisplayBlock();
 			}).dimensions(90, 150, 20, 20).build();
 
 			this.increaseYaw = ButtonWidget.builder(Text.literal("+"), action -> {
-				this.displayBlock.yaw += 0.0174533F;
+				this.displayBlock.yaw += pitchYawChange;
 				this.displayBlock.yaw = MathHelper.clamp(this.displayBlock.yaw, -6.28319F,6.28319F);
 				editItemDisplayBlock();
 			}).dimensions(110, 150, 20, 20).build();
@@ -125,12 +128,16 @@ public class ItemDisplayBlockEditScreen extends GlowcaseScreen {
 	public void render(DrawContext context, int mouseX, int mouseY, float delta) {
 		if(this.client != null) {
 			super.render(context, mouseX, mouseY, delta);
+
+			int degreesPitch = (int) (15*Math.round((this.displayBlock.pitch*(180F/Math.PI)/15)));
+			int degreesYaw = (int) (15*Math.round((this.displayBlock.yaw*(180F/Math.PI)/15)));
+
 			context.drawTextWithShadow(client.textRenderer, Text.translatable("gui.glowcase.scale_value", this.displayBlock.scale), 7, 7, 0xFFFFFFFF);
 			context.drawTextWithShadow(client.textRenderer, Text.translatable("gui.glowcase.x_offset_value", this.displayBlock.xOffset), 7, 37, 0xFFFFFFFF);
 			context.drawTextWithShadow(client.textRenderer, Text.translatable("gui.glowcase.y_offset_value", this.displayBlock.yOffset), 7, 67, 0xFFFFFFFF);
 			context.drawTextWithShadow(client.textRenderer, Text.translatable("gui.glowcase.z_offset_value", this.displayBlock.zOffset), 7, 97, 0xFFFFFFFF);
-			context.drawTextWithShadow(client.textRenderer, Text.translatable("gui.glowcase.pitch_value", MathHelper.floor(this.displayBlock.pitch*(180F/Math.PI))), 7, 127, 0xFFFFFFFF);
-			context.drawTextWithShadow(client.textRenderer, Text.translatable("gui.glowcase.yaw_value", MathHelper.floor(this.displayBlock.yaw*(180F/Math.PI))), 7, 157, 0xFFFFFFFF);
+			context.drawTextWithShadow(client.textRenderer, Text.translatable("gui.glowcase.pitch_value", degreesPitch), 7, 127, 0xFFFFFFFF);
+			context.drawTextWithShadow(client.textRenderer, Text.translatable("gui.glowcase.yaw_value", degreesYaw), 7, 157, 0xFFFFFFFF);
 		}
 	}
 
