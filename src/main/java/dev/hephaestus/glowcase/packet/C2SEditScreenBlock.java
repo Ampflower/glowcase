6bb00376c90ee8e8fb473b42ced13a2396731f6f
dooji2 <doojisbasement@gmail.com>
2025-04-08 22:48:21 +0300

    add new precise offset variables to packet

--- src/main/java/dev/hephaestus/glowcase/packet/C2SEditScreenBlock.java
+++ src/main/java/dev/hephaestus/glowcase/packet/C2SEditScreenBlock.java
@@ -10,7 +10,7 @@ import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 
-public record C2SEditScreenBlock(BlockPos pos, float width, float height, ScreenBlockEntity.Offset xOffset, ScreenBlockEntity.Offset yOffset, ScreenBlockEntity.Offset zOffset, boolean eink, boolean stretch, String url, String alt) implements C2SEditBlockEntity {
+public record C2SEditScreenBlock(BlockPos pos, float width, float height, ScreenBlockEntity.Offset xOffset, ScreenBlockEntity.Offset yOffset, ScreenBlockEntity.Offset zOffset, boolean eink, boolean stretch, String url, String alt, float preciseX, float preciseY, float preciseZ) implements C2SEditBlockEntity {
 	public static final Id<C2SEditScreenBlock> ID = new Id<>(Glowcase.id("channel.screen_block"));
 	public static final PacketCodec<RegistryByteBuf, C2SEditScreenBlock> PACKET_CODEC = PacketCodec.of(
 		(packet, buf) -> {
@@ -26,6 +26,9 @@ public record C2SEditScreenBlock(BlockPos pos, float width, float height, Screen
 			PacketCodecs.BOOL.encode(buf, packet.stretch);
 			PacketCodecs.STRING.encode(buf, trimmed.getFirst());
 			PacketCodecs.STRING.encode(buf, trimmed.getSecond());
+			PacketCodecs.FLOAT.encode(buf, packet.preciseX);
+            PacketCodecs.FLOAT.encode(buf, packet.preciseY);
+            PacketCodecs.FLOAT.encode(buf, packet.preciseZ);
 		},
 		(buf) -> new C2SEditScreenBlock(BlockPos.PACKET_CODEC.decode(buf),
 			PacketCodecs.FLOAT.decode(buf),
@@ -36,11 +39,14 @@ public record C2SEditScreenBlock(BlockPos pos, float width, float height, Screen
 			PacketCodecs.BOOL.decode(buf),
 			PacketCodecs.BOOL.decode(buf),
 			PacketCodecs.STRING.decode(buf),
-			PacketCodecs.STRING.decode(buf))
+			PacketCodecs.STRING.decode(buf),
+			PacketCodecs.FLOAT.decode(buf),
+            PacketCodecs.FLOAT.decode(buf),
+            PacketCodecs.FLOAT.decode(buf))
 	);
 
 	public static C2SEditScreenBlock of(ScreenBlockEntity be) {
-		return new C2SEditScreenBlock(be.getPos(), be.width, be.height, be.xOffset, be.yOffset, be.zOffset, be.eink, be.stretch, be.url, be.alt);
+		return new C2SEditScreenBlock(be.getPos(), be.width, be.height, be.xOffset, be.yOffset, be.zOffset, be.eink, be.stretch, be.url, be.alt, be.preciseX, be.preciseY, be.preciseZ);
 	}
 
 	@Override
@@ -50,6 +56,11 @@ public record C2SEditScreenBlock(BlockPos pos, float width, float height, Screen
 		Pair<String, String> trimmed = ScreenBlockEntity.trimStr(url, alt);
 
 		be.setupScreen(this.width, this.height, this.xOffset, this.yOffset, this.zOffset, this.eink, this.stretch);
+
+		be.preciseX = this.preciseX;
+        be.preciseY = this.preciseY;
+        be.preciseZ = this.preciseZ;
+
 		be.setImage(trimmed.getFirst(), trimmed.getSecond(), null); // Does markDirty and dispatch for us
 	}
 
