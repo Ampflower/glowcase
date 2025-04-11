f7e744ca3d16bdd1c0441584f8c5349ed3169cea
dooji2 <doojisbasement@gmail.com>
2025-04-11 22:37:47 +0300

    add rotation stuff to payload

--- src/main/java/dev/hephaestus/glowcase/packet/C2SEditRecipeBlock.java
+++ src/main/java/dev/hephaestus/glowcase/packet/C2SEditRecipeBlock.java
@@ -11,17 +11,19 @@ import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 
-public record C2SEditRecipeBlock(BlockPos pos, String recipe, TextBlockEntity.ZOffset offset) implements C2SEditBlockEntity {
+public record C2SEditRecipeBlock(BlockPos pos, String recipe, TextBlockEntity.ZOffset offset, float rotationX, float rotationY) implements C2SEditBlockEntity {
 	public static final Id<C2SEditRecipeBlock> ID = new Id<>(Glowcase.id("channel.recipe.save"));
 	public static final PacketCodec<RegistryByteBuf, C2SEditRecipeBlock> PACKET_CODEC = PacketCodec.tuple(
 		BlockPos.PACKET_CODEC, C2SEditRecipeBlock::pos,
 		PacketCodecs.STRING, C2SEditRecipeBlock::recipe,
 		PacketCodecs.INTEGER.xmap(index -> TextBlockEntity.ZOffset.values()[index], TextBlockEntity.ZOffset::ordinal), C2SEditRecipeBlock::offset,
+		PacketCodecs.FLOAT, C2SEditRecipeBlock::rotationX,
+		PacketCodecs.FLOAT, C2SEditRecipeBlock::rotationY,
 		C2SEditRecipeBlock::new
 	);
 
 	public static C2SEditRecipeBlock of(RecipeBlockEntity be) {
-		return new C2SEditRecipeBlock(be.getPos(), be.recipe, be.zOffset);
+		return new C2SEditRecipeBlock(be.getPos(), be.recipe, be.zOffset, be.rotationX, be.rotationY);
 	}
 
 	@Override
@@ -35,6 +37,8 @@ public record C2SEditRecipeBlock(BlockPos pos, String recipe, TextBlockEntity.ZO
 
 		be.setRecipe(this.recipe());
 		be.zOffset = this.offset();
+		be.rotationX = this.rotationX();
+		be.rotationY = this.rotationY();
 
 		be.markDirty();
 	}
