b55987448b7fccf69f80c353f612f5667fd5f22d
dooji2 <doojisbasement@gmail.com>
2025-04-12 20:43:41 +0300

    add renderAsBlock checkbox to item edit screen

--- src/main/java/dev/hephaestus/glowcase/client/gui/screen/ingame/ItemDisplayEditScreen.java
+++ src/main/java/dev/hephaestus/glowcase/client/gui/screen/ingame/ItemDisplayEditScreen.java
@@ -2,11 +2,28 @@ package dev.hephaestus.glowcase.client.gui.screen.ingame;
 
 import dev.hephaestus.glowcase.block.entity.DisplayBlockEntity;
 import dev.hephaestus.glowcase.packet.C2SEditItemDisplayBlock;
+import net.minecraft.client.gui.widget.CheckboxWidget;
+import net.minecraft.text.Text;
 
 public class ItemDisplayEditScreen extends DisplayBlockEditScreen {
+	private CheckboxWidget renderAsBlockWidget;
+
 	public ItemDisplayEditScreen(DisplayBlockEntity displayBlock) {
 		super(displayBlock);
 	}
+
+	@Override
+    public void init() {
+        super.init();
+
+        this.renderAsBlockWidget = CheckboxWidget.builder(Text.translatable("gui.glowcase.render_as_block"), this.client.textRenderer)
+			.checked(this.displayBlock.getRenderAsBlock())
+			.callback((checkbox, checked) -> this.displayBlock.setRenderAsBlock(checked))
+			.pos(20, 197)
+			.build();
+        
+        this.addDrawableChild(this.renderAsBlockWidget);
+    }
 
 	@Override
 	protected void editDisplayBlock() {
